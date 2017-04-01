77f8eaff16a04e8833d3b6b15ebb5a505d770d70
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-04-01 17:26:14 -0600

    refactor plugin\users code to be more static,. centers around 1 user

--- platform/web/app/plugins/think-shift/resources/controllers/Users.php
+++ platform/web/app/plugins/think-shift/resources/controllers/Users.php
@@ -3,24 +3,50 @@ namespace ThinkShift\Plugin;
 
 
 class Users extends Base {
-	private static $infusionsoft;
+	private static $infusionsoft, $contactId, $user, $userId;
 
 
 	public function init() {
 		parent::init();
 
 		self::$infusionsoft = new \ThinkShift\Plugin\Infusionsoft();
+        self::$contactId = self::getContactId();
+        self::setUserId();
 
-		add_action( 'wp_login', array( $this, 'wpLogin' ), 10, 2 );
-		add_action( 'wp_register', array( $this, 'wpRegister' ), 1 );
+		add_action( 'wp_login', array( $this, 'wpLogin' ), 20, 2 );
+		//add_action( 'wp_register', array( $this, 'wpRegister' ), 1 );
+
+
 
 	}
 
 
+	public static function setUserId( $userId = null ) {
+	    if( is_int($userId) ) {
+            self::$userId = $userId;
+            if( $user = get_user_by( 'id', $userId ) )
+                self::$user = $user;
+        } elseif( is_user_logged_in() ) {
+            self::$userId = self::$user->ID;
+            self::$user = get_current_user();
+        }
 
-	/*
-	 * Runs on successful user Log in
-	 */
+        self::$contactId = get_user_meta( self::$userId, 'infusionsoft_id', true );
+    }
+
+
+	public static function getContactId() {
+	    if( !self::$contactId ) {
+            $contactId = get_user_meta( self::$userId, 'infusionsoft_id', true );
+        } else {
+	        # todo: check on IS
+            $contactId = null;
+        }
+        return (int) $contactId;
+
+    }
+
+
 	public function wpLogin( $user_login, $user ) {
 		#if ( current_user_can( 'subscriber' ) ) {
 			$fields = $this->parseFields( $user->ID );
@@ -28,19 +54,17 @@ class Users extends Base {
 		#}
 	}
 
-	/*
-	 * Runs on successful user registration
-	 */
-	public function wpRegister( $userId ) {
-			$fields = $this->parseFields( $userId );
-			$this->addInfusionsoftContact( $userId, $fields );
+
+	public function wpRegister() {
+			$fields = $this->parseFields( self::$userId );
+			$this->addInfusionsoftContact( self::$userId, $fields );
 	}
 
 
-	public function parseFields( $userId ) {
+	public function parseFields() {
 		# if regular login
 		#if( true ) {
-			$userMeta = get_userdata( $userId );
+			$userMeta = get_userdata( self::$userId );
 			$fields = array(
 				'FirstName' => $userMeta->first_name,
 				'LastName' => $userMeta->last_name,
@@ -54,25 +78,29 @@ class Users extends Base {
 	}
 
 
-	public function addInfusionsoftContact( $userId, $fields ) {
-
+	public function addInfusionsoftContact( $fields = [] ) {
 		$contactId = self::$infusionsoft->addContact( $fields );
-		if( $contactId )
-			update_user_meta( $userId, 'infusionsoft_id', $contactId );
+
+		if( $contactId ) {
+            update_user_meta( self::$userId, 'infusionsoft_id', $contactId );
+            self::$contactId = $contactId;
+        }
 
 	}
 
 
-	public static function getUserTags( $userId ) {
-		$contactId = get_user_meta( $userId, 'infusionsoft_id', true );
+    public static function getUserTags( ) {
+        $contactId = self::getContactId();
 
-		# todo: save tags somewhere later
-		if( $contactId )
-			return self::$infusionsoft->getTagsByContactId( $contactId );
-		else
-			return false;
+        # todo: save tags somewhere later
+        if( $contactId )
+            return self::$infusionsoft->getTagsByContactId( $contactId );
+        else
+            return false;
 
-	}
+    }
+
+
 
 
 }
