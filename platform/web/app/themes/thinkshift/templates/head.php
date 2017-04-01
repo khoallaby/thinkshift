a0ea6ea97491fe6b3a39df22236fb94eb935a9bd
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-04-01 13:17:48 -0600

    changes to header, to match mock

--- platform/web/app/themes/thinkshift/templates/head.php
+++ platform/web/app/themes/thinkshift/templates/head.php
@@ -1,6 +1,11 @@
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <?php wp_head(); ?>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <title><?php bloginfo( 'title'); ?></title>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
+	<?php wp_head(); ?>
 </head>
--- platform/web/app/themes/thinkshift/templates/header.php
+++ platform/web/app/themes/thinkshift/templates/header.php
@@ -1,12 +1,6 @@
 <header class="banner">
   <div class="container">
     <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
+    <?php get_template_part( 'templates/menu' ); ?>
   </div>
 </header>
