9db475de61c3bd71d1f41cb4e0c4a49b94a68a2c
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-04-01 17:27:59 -0600

    add documentation to Plugin\Users class

--- platform/web/app/plugins/think-shift/resources/controllers/Users.php
+++ platform/web/app/plugins/think-shift/resources/controllers/Users.php
@@ -3,26 +3,30 @@ namespace ThinkShift\Plugin;
 
 
 class Users extends Base {
-	private static $infusionsoft, $contactId, $user, $userId;
+    private static $infusionsoft, $contactId, $user, $userId;
 
 
-	public function init() {
-		parent::init();
+    public function init() {
+        parent::init();
 
-		self::$infusionsoft = new \ThinkShift\Plugin\Infusionsoft();
+        self::$infusionsoft = new \ThinkShift\Plugin\Infusionsoft();
         self::$contactId = self::getContactId();
         self::setUserId();
 
-		add_action( 'wp_login', array( $this, 'wpLogin' ), 20, 2 );
-		//add_action( 'wp_register', array( $this, 'wpRegister' ), 1 );
+        add_action( 'wp_login', array( $this, 'wpLogin' ), 20, 2 );
+        //add_action( 'wp_register', array( $this, 'wpRegister' ), 1 );
 
 
 
-	}
+    }
 
 
-	public static function setUserId( $userId = null ) {
-	    if( is_int($userId) ) {
+    /**
+     * Sets up our user/userID variables. If user is logged in, use current user.
+     * @param null $userId
+     */
+    public static function setUserId( $userId = null ) {
+        if( is_int($userId) ) {
             self::$userId = $userId;
             if( $user = get_user_by( 'id', $userId ) )
                 self::$user = $user;
@@ -35,11 +39,15 @@ class Users extends Base {
     }
 
 
-	public static function getContactId() {
-	    if( !self::$contactId ) {
+    /**
+     * Retrieves Infusionsoft's contact ID, stored in usermeta
+     * @return int Infusionsoft's contact ID
+     */
+    public static function getContactId() {
+        if( !self::$contactId ) {
             $contactId = get_user_meta( self::$userId, 'infusionsoft_id', true );
         } else {
-	        # todo: check on IS
+            # todo: check on IS
             $contactId = null;
         }
         return (int) $contactId;
@@ -47,48 +55,68 @@ class Users extends Base {
     }
 
 
-	public function wpLogin( $user_login, $user ) {
-		#if ( current_user_can( 'subscriber' ) ) {
-			$fields = $this->parseFields( $user->ID );
-			$this->addInfusionsoftContact( $user->ID, $fields );
-		#}
-	}
+    /**
+     * Runs on (successful?) user Log in
+     * @param $user_login
+     * @param $user
+     */
+    public function wpLogin( $user_login, $user ) {
+        #if ( current_user_can( 'subscriber' ) ) {
+        $fields = $this->parseFields( $user->ID );
+        $this->addInfusionsoftContact( $user->ID, $fields );
+        #}
+    }
 
 
-	public function wpRegister() {
-			$fields = $this->parseFields( self::$userId );
-			$this->addInfusionsoftContact( self::$userId, $fields );
-	}
+    /**
+     * Runs on successful user registration
+     */
+    public function wpRegister() {
+        $fields = $this->parseFields( self::$userId );
+        $this->addInfusionsoftContact( self::$userId, $fields );
+    }
 
 
-	public function parseFields() {
-		# if regular login
-		#if( true ) {
-			$userMeta = get_userdata( self::$userId );
-			$fields = array(
-				'FirstName' => $userMeta->first_name,
-				'LastName' => $userMeta->last_name,
-				'Email' => $userMeta->user_email
-			);
-		#}
-		# todo: add parse for social media logins
+    /**
+     * Parses data from our user and compiles it to the right field names. Then sends it to infusionsoft
+     * @return array Fields with [infusionsoft-key] => [usermeta name]
+     */
+    public function parseFields() {
+        # if regular login
+        #if( true ) {
+        $userMeta = get_userdata( self::$userId );
+        $fields = array(
+            'FirstName' => $userMeta->first_name,
+            'LastName' => $userMeta->last_name,
+            'Email' => $userMeta->user_email
+        );
+        #}
+        # todo: add parse for social media logins
 
-		return $fields;
+        return $fields;
 
-	}
+    }
 
 
-	public function addInfusionsoftContact( $fields = [] ) {
-		$contactId = self::$infusionsoft->addContact( $fields );
+    /**
+     * Adds a contact to Infusionsoft
+     * @param $fields Fields of user meta to change. In form of [infusionsoft-key] => [usermeta name]
+     */
+    public function addInfusionsoftContact( $fields = [] ) {
+        $contactId = self::$infusionsoft->addContact( $fields );
 
-		if( $contactId ) {
+        if( $contactId ) {
             update_user_meta( self::$userId, 'infusionsoft_id', $contactId );
             self::$contactId = $contactId;
         }
 
-	}
+    }
 
 
+    /**
+     * Gets all the tags and categories for a user
+     * @return array|bool Array of matching tag/categories
+     */
     public static function getUserTags( ) {
         $contactId = self::getContactId();
 
