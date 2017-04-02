e62c90a19c47f4c8dfd3d4f8bd09d503d12cb9c1
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-04-02 00:37:11 -0600

    move templates/show-tags into this file

--- platform/web/app/themes/thinkshift/templates/dashboard/card-my-strengths.php
+++ platform/web/app/themes/thinkshift/templates/dashboard/card-my-strengths.php
@@ -2,8 +2,18 @@
     <div class="card-block">
         <h6 class="mb-3">My Strength's</h6>
 
-        <ul class="list-unstyled list-spaced">
-            <?php echo do_shortcode( '[ts_show_tags]' ); ?>
-        </ul>
+        <?php
+        $users = \ThinkShift\Plugin\Users::get_instance();
+        $tags = $users::getUserStrengths();
+        if( $tags ) {
+            ?>
+            <ul class="list-unstyled list-spaced user-tags">
+                <?php foreach( $tags as $tag ) { ?>
+                    <li><span class="text-muted icon mr-3"></span> <?= $tag; ?></li>
+                <?php } ?>
+            </ul>
+            <?php
+        }
+        ?>
     </div>
 </div>
