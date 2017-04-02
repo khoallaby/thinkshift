4727d072aa82c2f7568099717924ad6f92dd7103
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-04-01 21:40:11 -0600

    bunch more fixes to get this working properly

--- platform/web/app/plugins/think-shift/resources/controllers/Users.php
+++ platform/web/app/plugins/think-shift/resources/controllers/Users.php
@@ -13,9 +13,10 @@ class Users extends Base {
         self::$infusionsoft = new \ThinkShift\Plugin\Infusionsoft();
         self::$contactId = self::getContactId();
         #var_dump( is_user_logged_in() );
-        self::setUserId();
+        add_action('init', array( __CLASS__, 'setUserId' ) );
+        //self::setUserId();
 
-        add_action( 'wp_login', array( $this, 'wpLogin' ), 20, 2 );
+        //add_action( 'wp_login', array( $this, 'wpLogin' ), 200, 2 );
         //add_action( 'wp_register', array( $this, 'wpRegister' ), 1 );
 
     }
@@ -41,6 +42,15 @@ class Users extends Base {
         #if ( current_user_can( 'subscriber' ) ) {
         $fields = $this->parseFields( $user->ID );
         $this->addInfusionsoftContact( $user->ID, $fields );
+        self::setUserId( $user->ID );
+
+
+        // update user's "strengths" metadata
+        $tags = self::getUserTagsByCategory( 'MA Value Creation Strengths' ); // or 41
+        $i = 1;
+        #foreach( $tags as $k => $tag )
+            #update_user_meta( self::$userId, 'strength_' . $i, $tag['GroupName'] );
+
         #}
     }
 
@@ -71,9 +81,11 @@ class Users extends Base {
             self::$userId = $userId;
             if( $user = get_user_by( 'id', $userId ) )
                 self::$user = $user;
-        } elseif( is_user_logged_in() ) {
+        } elseif( function_exists( 'is_user_logged_in') && is_user_logged_in() ) {
             self::$user = wp_get_current_user();
             self::$userId = self::$user->ID;
+        } else {
+            self::$user = self::$userId = null;
         }
 
         self::$contactId = get_user_meta( self::$userId, 'infusionsoft_id', true );
@@ -89,7 +101,7 @@ class Users extends Base {
             $userId = self::$userId;
 
         if( !is_int(self::$contactId) ) {
-            $contactId = get_user_meta( $userId, 'infusionsoft_id', true );
+            self::$contactId = $contactId = get_user_meta( $userId, 'infusionsoft_id', true );
         } else {
             # todo: check on IS
             $contactId = null;
@@ -104,10 +116,11 @@ class Users extends Base {
      * Parses data from our user and compiles it to the right field names. Then sends it to infusionsoft
      * @return array Fields with [infusionsoft-key] => [usermeta name]
      */
-    public function parseFields() {
-        # if regular login
-        #if( true ) {
-        $userMeta = get_userdata( self::$userId );
+    public function parseFields( $userId = null ) {
+        if( !$userId )
+            $userId = self::$userId;
+
+        $userMeta = get_userdata( $userId );
         $fields = array(
             'FirstName' => $userMeta->first_name,
             'LastName' => $userMeta->last_name,
@@ -138,7 +151,10 @@ class Users extends Base {
      * Adds a contact to Infusionsoft
      * @param $fields Fields of user meta to change. In form of [infusionsoft-key] => [usermeta name]
      */
-    public function addInfusionsoftContact( $fields = [] ) {
+    public function addInfusionsoftContact( $userId = null, $fields = array() ) {
+        if( !$userId )
+            $userId = self::$userId;
+
         $contactId = self::$infusionsoft->addContact( $fields );
 
         if( $contactId ) {
@@ -219,4 +235,7 @@ class Users extends Base {
 
 }
 
+add_action( 'wp_head', array( \ThinkShift\Plugin\Users::get_instance(), 'init' ));
+add_action( 'wp_login', array( \ThinkShift\Plugin\Users::get_instance(), 'wpLogin' ), 200, 2 );
+//add_action( 'wp_register', array( $this, 'wpRegister' ), 1 );
 #add_action( 'init', array( \ThinkShift\Plugin\Users::get_instance(), 'init' ));
