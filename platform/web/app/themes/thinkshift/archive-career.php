c0eb93cdd0b326c6b050cf5a520f3e2c15a6266d
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-04-04 22:55:56 -0600

    fix show/hide no results msg

--- platform/web/app/themes/thinkshift/archive-career.php
+++ platform/web/app/themes/thinkshift/archive-career.php
@@ -1,11 +1,5 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
-<?php if (!have_posts()) : ?>
-    <div class="alert alert-warning">
-        <?php _e('Sorry, no results were found.', 'sage'); ?>
-    </div>
-    <?php get_search_form(); ?>
-<?php endif; ?>
 
 <!--FILTER FOR POST LOOP-->
 <div id="post-filter">
@@ -26,20 +20,28 @@
 </div>
 
 
-<?php while (have_posts()) : the_post(); ?>
-    <article <?php post_class(); ?>>
-        <header>
-            <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-        </header>
-        <div class="row">
-          <div class="col-lg-12">
-            <?php
-                get_template_part('templates/' . get_post_type() . '/index', 'occupation' );
-            ?>
-          </div>
-        </div>
-    </article>
-<?php
+<?php if (!have_posts()) : ?>
+    <div class="alert alert-warning">
+        <?php _e('Sorry, no results were found.', 'sage'); ?>
+    </div>
+    <?php get_search_form(); ?>
+<?php else : ?>
+    <?php while (have_posts()) : the_post(); ?>
+        <article <?php post_class(); ?>>
+            <header>
+                <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            </header>
+            <div class="row">
+              <div class="col-lg-12">
+                <?php
+                    get_template_part('templates/' . get_post_type() . '/index', 'occupation' );
+                ?>
+              </div>
+            </div>
+        </article>
+    <?php
 
-endwhile;
-the_posts_navigation();
+    endwhile;
+    the_posts_navigation();
+
+endif;
