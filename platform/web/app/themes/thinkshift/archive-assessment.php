26c05a1cce0dc5ccfca9913f73abf235eef40a04
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-04-07 12:52:16 -0600

    #18 - better logic for show/hide, show first if none are completed

--- platform/web/app/themes/thinkshift/archive-assessment.php
+++ platform/web/app/themes/thinkshift/archive-assessment.php
@@ -1,20 +1,33 @@
 <?php
+$assesments = [];
+foreach( $wp_query->get_posts() as $post ) {
+    $complete = \ThinkShift\Plugin\Users::userHasTag(
+        get_post_meta( get_the_ID(), 'tag-complete', true )
+    );
+    $incomplete = \ThinkShift\Plugin\Users::userHasTag(
+        get_post_meta( get_the_ID(), 'tag-incomplete', true )
+    );
+
+    $assesments[] = $complete;
+}
+
+# if none are completed,  show the first one
+if( array_sum($assesments) == 0 )
+    $assesments[0] = true;
+
+
 
 if( have_posts() ) :
-    $i = 1;
+    $i = 0;
     while (have_posts()) : the_post();
+        $show = $assesments[ $i ];
+        $i++;
 
         # checks if User has the in/complete Tag for the following assessment
-        $complete = \ThinkShift\Plugin\Users::userHasTag(
-            get_post_meta( get_the_ID(), 'tag-complete', true )
-        );
-        $incomplete = \ThinkShift\Plugin\Users::userHasTag(
-            get_post_meta( get_the_ID(), 'tag-incomplete', true )
-        );
 
 
-        if( $complete ) {
-            echo sprintf( '<h2 class="entry-title"><a href="%s" class="btn btn-primary disabled">%s</a></h2>', get_permalink(), get_the_title() );
+        if( $show ) {
+            echo sprintf( '<h2 class="entry-title"><a href="%s" class="btn btn-secondary">%s</a></h2>', get_permalink(), get_the_title() );
         } else {
             ?>
             <article <?php post_class(); ?>>
