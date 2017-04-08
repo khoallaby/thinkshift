a8d1e140cbda4747e57d68cac9c63ffa2fe43297
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-04-08 02:34:49 -0600

    #22 - update with video thumbnails

--- platform/web/app/themes/thinkshift/template-video.php
+++ platform/web/app/themes/thinkshift/template-video.php
@@ -23,26 +23,41 @@ while (have_posts()) : the_post();
         foreach( $videos as $k => $video ) :
             Template::echo_open_row( $i, $columns );
 
-            if( $video['video_source'] == 'youtube' )
+            if( $video['video_source'] == 'youtube' ) {
                 $url = 'https://www.youtube.com/embed/' . $video['video_url'];
-            elseif( $video['video_source'] == 'vimeo' )
+                $imgUrl = 'https://img.youtube.com/vi/' . $video['video_url'] . '/0.jpg';
+            } elseif( $video['video_source'] == 'vimeo' ) {
                 $url = 'https://player.vimeo.com/video/' . $video['video_url'];
-            else
+                # todo: vimeo urls -- https://gist.github.com/tjFogarty/08cddb1854e2ae593bf0
+                $imgUrl = '';
+            } else {
                 $url = $video['video_url'];
+                $imgUrl = '';
+            }
+
+
+            if( isset($imgUrl) ) {
+                $link = '<img src="' . esc_attr( $imgUrl ) . '" />';
+            } else {
+                $link = $video['video_url'];
+            }
 
             ?>
             <div class="col-md-4">
 
-                <a href="#video-<?php echo esc_attr( $k ); ?>" class="btn btn-lg btn-primary" data-toggle="modal">Video link</a>
 
-                <div id="video-<?php echo esc_attr( $k ); ?>" class="modal fade">
+                <a href="#modal-video-<?php echo esc_attr( $k ); ?>" data-toggle="modal"><?php echo $link; ?></a>
+
+                <div id="modal-video-<?php echo esc_attr( $k ); ?>" class="modal fade modal-video">
                     <div class="modal-dialog">
                         <div class="modal-content">
+                            <!--
                             <div class="modal-header">
                                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
                             </div>
+                            -->
                             <div class="modal-body">
-                                <iframe width="100%" style="height: auto; min-height: 300px;" src="<?php echo esc_attr( $url ); ?>" data-src="<?php echo esc_attr( $url ); ?>" frameborder="0" allowfullscreen></iframe>
+                                <iframe src="<?php echo esc_attr( $url ); ?>" data-src="<?php echo esc_attr( $url ); ?>" frameborder="0" allowfullscreen></iframe>
                             </div>
                         </div>
                     </div>
