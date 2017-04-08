7ae46bbee2e97919a254df8b4f33fc2622cdb11e
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-04-08 16:52:57 -0600

    #22 - change the way meta boxes are saved on videos

--- platform/web/app/plugins/think-shift/parts/meta-boxes/page-video.php
+++ platform/web/app/plugins/think-shift/parts/meta-boxes/page-video.php
@@ -1,59 +1,34 @@
 <?php
 /*
 Title: Video Options
-Post Type: page
+Post Type: video
 Order: 1
 Collapse: false
 Flow: Video
-Template: template-video.php
 */
-
-
-use ThinkShift\Plugin\Tags;
 
 
 
 piklist( 'field', array(
-    'type'        => 'group',
-    'field'    => 'videos',
-    'label'       => __( 'Videos', 'thinkshift' ),
-    'description' => __( 'Add videos', 'thinkshift' ),
-    'add_more'    => true,
-    'fields'      => array(
-        array(
-            'type'    => 'text',
-            'field'   => 'video_url',
-            'label'   => __( 'URL', 'thinkshift,' ),
-            'columns' => 12
-        ),
-        array(
-            'type'    => 'select',
-            'field'   => 'video_source',
-            #'scope'   => 'taxonomy',
-            'label'   => __( 'Video Source', 'thinkshift,' ),
-            'columns' => 12,
-            'choices' => [
-                '' => '',
-                'vimeo' => 'Vimeo',
-                'youtube' => 'YouTube'
-            ]
-        ),
-        array(
-            'type'    => 'select',
-            'field'   => 'video_tag',
-            #'scope'   => 'taxonomy',
-            'label'   => __( 'Strengths', 'thinkshift,' ),
-            'columns' => 12,
-            'choices' => piklist(
-                Tags::getTagsFromCategory( Tags::$strengthMetaKey ),
-                array(
-                    'term_id',
-                    'name'
-                )
-            ),
-            'attributes' => array(
-                'multiple' => 'multiple' // This changes a select field into a multi-select field
-            )
-        )
+    'type'       => 'text',
+    'field'      => 'video_url',
+    'label'      => __( 'URL ID', 'thinkshift' ),
+    'help'       => __( 'Use the unique ID found at the end of the URL. Example: "dQw4w9WgXcQ" from -- www.youtube.com/watch?v=dQw4w9WgXcQ', 'thinkshift' ),
+    'attributes' => array(
+        'class' => 'regular-text'
+    )
+) );
+
+piklist( 'field', array(
+    'type'       => 'select',
+    'field'   => 'video_source',
+    'label'   => __( 'Video Source', 'thinkshift,' ),
+    'choices' => [
+        '' => '',
+        'vimeo' => 'Vimeo',
+        'youtube' => 'YouTube'
+    ],
+    'attributes' => array(
+        'class' => 'regular-text'
     )
 ) );
