9ad3ff963765efde2baa814b912a6a74793d8475
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-04-08 23:55:46 -0600

    #27 - convert career filter to use taxonomy IDs

--- platform/web/app/themes/thinkshift/archive-career.php
+++ platform/web/app/themes/thinkshift/archive-career.php
@@ -1,43 +1,13 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<?php
+use ThinkShift\Plugin\Tags;
+use ThinkShift\Plugin\Videos;
 
 
-    <form id="post-filter">
-        <div class="row">
-            <div class="col-12 text-xs-center" data-toggle="buttons">
-                <?php
+get_template_part( 'templates/page', 'header' );
+$strengths = Tags::getAllStrengths( false );
+get_template_part( 'templates/shared/header', 'strengths-filter' );
+?>
 
-                if( isset($_GET['strengths']) )
-                    $strengthArray = $_GET['strengths'];
-                else
-                    $strengthArray = ThinkShift\Plugin\Users::getUserStrengths();
-                if( !$strengthArray )
-                    $strengthArray = [];
-
-
-                $strengths = ThinkShift\Plugin\Tags::getAllStrengths( false );
-
-                foreach( $strengths as $strength ) {
-
-                    $checked = in_array( $strength, $strengthArray );
-                    ?>
-                    <label class="btn mr-4 mb-4 btn-lg btn-pill btn-primary <?php echo $checked ? 'active' : ''; ?>">
-                        <?php
-                        echo sprintf('<input type="checkbox" %s class="" autocomplete="off" name="strengths[]" value="%s"/> %s',
-                            $checked ? 'checked' : '',
-                            $strength,
-                            $strength
-                        );
-                        ?>
-                    </label>
-                <?php } ?>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-12 text-xs-right">
-                <input type="submit" class="btn btn-primary" value="Submit" />
-            </div>
-        </div>
-    </form>
 
     <ul id="og-grid" class="og-grid">
       <li>
