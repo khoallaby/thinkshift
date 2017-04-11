46b27cec073797e35173d81133c55d4bdce2e359
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-04-11 02:23:31 -0600

    fix wrong tags being retrieved in /careers loop

--- platform/web/app/themes/thinkshift/templates/career/index-occupation.php
+++ platform/web/app/themes/thinkshift/templates/career/index-occupation.php
@@ -1,5 +1,7 @@
 <?php
 
+use ThinkShift\Plugin\Users;
+
 $keys = ['ValueType1', 'ValueType2','ValueType3','occcode', 'occup', 'occdesc', 'alttitle1', 'alttitle2', 'alttitle3',
     'alttitle4', 'WorkActivity1', 'WorkActivity2','WorkActivity3', 'WorkActivity4',
     'WorkActivity5', 'WorkActivity6','WorkActivity7', 'EducationNorm', 'EducationMin', 'techskillkn1', 'techskillkn2',
@@ -7,14 +9,19 @@ $keys = ['ValueType1', 'ValueType2','ValueType3','occcode', 'occup', 'occdesc', 
     'openingscount', 'openingsrate', 'openingsratecat', 'pctselfemp',
     'pctselfempcat', 'highoppjobfamily', 'jobzone'];
 
-    echo sprintf('
-      <a class="job-title"
-      href="' . get_the_permalink() .'"
-      data-largesrc="images/1.jpg"
-      data-ValueType2="'.get_post_meta( get_the_ID(), 'ValueType2', true).'"
-      data-title="' . get_the_permalink() .'"
-      data-description="Swiss chard pumpkin bunya nuts maize plantain aubergine napa cabbage soko coriander sweet pepper water spinach winter purslane shallot tigernut lentil beetroot.">
-        <h1>' . get_the_title() . '</h1>
-      </a>
-    ');
+
+$strengths = Users::getObjStrengths( get_the_ID() );
+$strengthIds = array_keys($strengths);
+
+
+echo sprintf('
+<a class="job-title"
+    href="' . get_the_permalink() . '"
+    data-largesrc="images/1.jpg"
+    data-strengths="' . implode( ',', $strengthIds ) . '"
+    data-title="' . get_the_permalink() .'"
+    data-description="' . the_content() . '">
+    <h1>' . get_the_title() . '</h1>
+</a>
+');
 ?>
