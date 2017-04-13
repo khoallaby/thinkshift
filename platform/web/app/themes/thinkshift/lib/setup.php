f1defca580818f3446991e94c6b2a85a138d836d
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-04-13 13:36:57 -0600

    show different styles for different templates

--- platform/web/app/themes/thinkshift/lib/setup.php
+++ platform/web/app/themes/thinkshift/lib/setup.php
@@ -96,7 +96,10 @@ function display_sidebar() {
  * Theme assets
  */
 function assets() {
-  wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css'), false, null);
+if( is_page_template( 'template-external.php' ) )
+    wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css'), false, null);
+else
+    wp_enqueue_style('sage/css', Assets\asset_path('styles/main-external.css'), false, null);
 
   if (is_single() && comments_open() && get_option('thread_comments')) {
     wp_enqueue_script('comment-reply');
