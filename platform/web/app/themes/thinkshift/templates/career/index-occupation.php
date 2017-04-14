14a8dfcc1b12a2c2f795f54f86e6c55ae32094b1
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-04-14 02:06:50 -0600

    clean up occupation template

--- platform/web/app/themes/thinkshift/templates/career/index-occupation.php
+++ platform/web/app/themes/thinkshift/templates/career/index-occupation.php
@@ -8,55 +8,38 @@ $keys = ['alt_title1', 'alt_title2', 'alt_title3',
     'tech_skill_kn3', 'tech_skill_kn4', 'med_wage', 'openings_count', 'openings_rate', 'openings_rate_cat', 'pct_self_emp',
     'pct_self_emp_cat'];
 
-$strengths = Users::getObjStrengths( get_the_ID() );
-$strengthIds = array_keys($strengths);
+
 
 $i = 0;
-$strengthString = '';
+$strengthString = $dataString = '';
+$strengths = Users::getObjStrengths( get_the_ID() );
+$strengthIds = array_keys($strengths);
+$image = 'images/1.jpg';
 
+
+
+
+
+# generate data attributes for strengths
 foreach( $strengths as $k => $strength ) {
     $i++;
-    $strengthString .= 'data-valuetype' . $i . '="' . $strength . '" ';
+    $strengthString .= '      data-valuetype' . $i . '="' . $strength . '" ' . "\n";
 }
+
+# generate the rest of data attributes
+foreach( $keys as $key )
+    $dataString .= '      data-' . $key . '="' . esc_attr(get_post_meta( get_the_ID(), $key, true )) . '" ' . "\n";
+
+
 
 echo sprintf('
   <a class="job-title"
       href="' . get_the_permalink() . '"
-      '.$strengthString.'
-      data-work_activity1="'. get_post_meta( get_the_ID(), 'work_activity1', true) .'"
-      data-work_activity2="'. get_post_meta( get_the_ID(), 'work_activity2', true) .'"
-      data-work_activity3="'. get_post_meta( get_the_ID(), 'work_activity3', true) .'"
-      data-work_activity4="'. get_post_meta( get_the_ID(), 'work_activity4', true) .'"
-      data-work_activity5="'. get_post_meta( get_the_ID(), 'work_activity5', true) .'"
-      data-work_activity6="'. get_post_meta( get_the_ID(), 'work_activity6', true) .'"
-      data-work_activity7="'. get_post_meta( get_the_ID(), 'work_activity7', true) .'"
-
-      data-tech_skill_kn1="'. get_post_meta( get_the_ID(), 'tech_skill_kn1', true) .'"
-      data-tech_skill_kn2="'. get_post_meta( get_the_ID(), 'tech_skill_kn2', true) .'"
-      data-tech_skill_kn3="'. get_post_meta( get_the_ID(), 'tech_skill_kn3', true) .'"
-      data-tech_skill_kn4="'. get_post_meta( get_the_ID(), 'tech_skill_kn4', true) .'"
-
-      data-education_norm="'. get_post_meta( get_the_ID(), 'education_norm', true) .'"
-      data-education_min="'. get_post_meta( get_the_ID(), 'education_min', true) .'"
-
-      data-med_wage="'. get_post_meta( get_the_ID(), 'med_wage', true) .'"
-
-      data-openings_count="'. get_post_meta( get_the_ID(), 'openings_count', true) .'"
-      data-openings_rate="'. get_post_meta( get_the_ID(), 'openings_rate', true) .'"
-      data-openings_rate_cat="'. get_post_meta( get_the_ID(), 'openings_rate_cat', true) .'"
-
-      data-pct_self_emp="'. get_post_meta( get_the_ID(), 'pct_self_emp', true) .'"
-      data-pct_self_emp_cat="'. get_post_meta( get_the_ID(), 'pct_self_emp_cat', true) .'"
-
-      data-alt_title1="'. get_post_meta( get_the_ID(), 'alt_title1', true) .'"
-      data-alt_title2="'. get_post_meta( get_the_ID(), 'alt_title2', true) .'"
-      data-alt_title3="'. get_post_meta( get_the_ID(), 'alt_title3', true) .'"
-      data-alt_title4="'. get_post_meta( get_the_ID(), 'alt_title4', true) .'"
-
-      data-largesrc="images/1.jpg"
+      ' . $strengthString . $dataString . '
+      data-largesrc="' . esc_attr( $image  ). '"
       data-strengths="' . implode( ',', $strengthIds ) . '"
-      data-title="' . get_the_title() .'"
-      data-description="' . get_the_content() . '">
+      data-title="' . esc_attr( get_the_title() ) .'"
+      data-description="' . esc_attr( get_the_content() ) . '">
       <h1>' . get_the_title() . '</h1>
   </a>
 ');
