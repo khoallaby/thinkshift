7590480335fc7b9e295bc85e170a46287e0de9b5
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-04-14 16:00:14 -0600

    check for variable, to remove errors

--- platform/web/app/themes/thinkshift/templates/shared/header-filter-order.php
+++ platform/web/app/themes/thinkshift/templates/shared/header-filter-order.php
@@ -8,11 +8,14 @@ $order = [
     'asc' => 'Lowest to Highest',
     'desc' => 'Highest to Lowest'
 ];
+
 ?>
 <select name="orderby">
     <?php foreach( $orderby as $k => $ob ) {
         echo sprintf( '<option value="%s" %s>%s</option>',
-            $k, selected( $_GET['orderby'], $k, false ), $ob
+            $k,
+            isset($_GET['orderby']) ? selected( $_GET['orderby'], $k, false ) : '',
+            $ob
         );
     }
     ?>
@@ -22,7 +25,9 @@ $order = [
 <select name="order">
     <?php foreach( $order as $k => $o ) {
         echo sprintf( '<option value="%s" %s>%s</option>',
-            $k, selected( $_GET['order'], $k, false ), $o
+            $k,
+            isset($_GET['order']) ? selected( $_GET['order'], $k, false ) : '',
+            $o
         );
     }
     ?>
