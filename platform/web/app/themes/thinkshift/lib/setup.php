004a6d8b67dfb06227183ffb51e4ea01b3606141
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-04-14 16:32:03 -0600

    #58 - enqueue bootstrap deps for external pages

--- platform/web/app/themes/thinkshift/lib/setup.php
+++ platform/web/app/themes/thinkshift/lib/setup.php
@@ -96,15 +96,24 @@ function display_sidebar() {
  * Theme assets
  */
 function assets() {
-if( is_page_template( 'template-external.php' ) )
-    wp_enqueue_style('sage/css', Assets\asset_path('styles/main-external.css'), false, null);
-else
-    wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css'), false, null);
+    wp_register_script( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js', ['jquery'], '4.0.0-alpha.6', true );
 
-  if (is_single() && comments_open() && get_option('thread_comments')) {
-    wp_enqueue_script('comment-reply');
-  }
 
-  wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
+    if ( is_page_template( 'template-external.php' ) || (is_front_page() && !is_user_logged_in() )) {
+        wp_enqueue_style( 'sage/css', Assets\asset_path( 'styles/main-external.css' ), false, null );
+        wp_enqueue_script( 'bootstrap' );
+        wp_enqueue_script( 'jquery-effects-core' );
+        wp_register_script( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js', ['jquery'], '4.0.0-alpha.6', true );
+    } else {
+        wp_enqueue_style( 'sage/css', Assets\asset_path( 'styles/main.css' ), false, null );
+    }
+
+
+    if ( is_single() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+    }
+
+    wp_enqueue_script( 'sage/js', Assets\asset_path( 'scripts/main.js' ), [ 'jquery' ], null, true );
 }
-add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
+
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
