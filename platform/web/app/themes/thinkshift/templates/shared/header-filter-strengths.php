6334d02b39b21cd294b19ed1cda8a22ebf56e57a
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-04-17 23:08:03 -0600

    #63 - indents

--- platform/web/app/themes/thinkshift/templates/shared/header-filter-strengths.php
+++ platform/web/app/themes/thinkshift/templates/shared/header-filter-strengths.php
@@ -9,38 +9,39 @@ if( !isset($strengths) )
 
 <form id="post-filter">
     <div class="row">
-      <div class="col-md-12">
-        <div class="card">
-          <div class="card-header">
-            <div data-toggle="buttons" class="text-center">
-                  <?php
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <div data-toggle="buttons" class="text-center">
+                        <?php
 
-                  if( isset($_GET['strengths']) )
-                      $strengthArray = $_GET['strengths'];
-                  else
-                      $strengthArray = is_user_logged_in() ? array_keys( Users::getUserStrengths() ) : [];
-                  if( !$strengthArray )
-                      $strengthArray = [];
+                        if ( isset( $_GET['strengths'] ) )
+                            $strengthArray = $_GET['strengths'];
+                        else
+                            $strengthArray = is_user_logged_in() ? array_keys( Users::getUserStrengths() ) : [];
+
+                        if ( ! $strengthArray )
+                            $strengthArray = [];
 
 
 
-                  foreach( $strengths as $strengthId => $strength ) {
-                      $checked = in_array( $strengthId, $strengthArray );
-                      ?>
-                      <label class="btn mr-4 mb-4 btn-xs btn-pill btn-default <?php echo $checked ? 'active' : ''; ?>">
-                          <?php
-                          echo sprintf('<input type="checkbox" %s class="" autocomplete="off" name="strengths[]" value="%s"/> %s',
-                              $checked ? 'checked' : '',
-                              $strengthId,
-                              $strength
-                          );
-                          ?>
-                      </label>
-                  <?php } ?>
+                        foreach ( $strengths as $strengthId => $strength ) {
+                            $checked = in_array( $strengthId, $strengthArray );
+                            ?>
+                            <label class="btn mr-4 mb-4 btn-xs btn-pill btn-default <?php echo $checked ? 'active' : ''; ?>">
+                                <?php
+                                echo sprintf( '<input type="checkbox" %s class="" autocomplete="off" name="strengths[]" value="%s"/> %s',
+                                    $checked ? 'checked' : '',
+                                    $strengthId,
+                                    $strength
+                                );
+                                ?>
+                            </label>
+                        <?php } ?>
+                    </div>
                 </div>
             </div>
-          </div>
-      </div>
+        </div>
     </div>
     <div class="row">
         <div class="col-12 text-xs-right">
