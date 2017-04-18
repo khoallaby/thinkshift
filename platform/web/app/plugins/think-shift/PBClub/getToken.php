6770696b7ff9b6412460fa57a86ccb15753b30c5
John <john@theplanbclub.com>
2017-04-17 20:39:32 -0700

    This file simply provided a read from the database containing the access token using the function getToken()

--- platform/web/app/plugins/think-shift/PBClub/getToken.php
+++ platform/web/app/plugins/think-shift/PBClub/getToken.php
@@ -6,6 +6,30 @@
  * Time: 7:54 PM
  */
 
+// back path to .env for db password
+$file=fopen('../../../../../.env','r');
+
+// read db pw to obfuscate
+/**
+ * @return bool|string
+ *      return string value of pw if found otherwise return false
+ */
+function mysqlPW() {
+    global $file;
+
+    while (!feof($file)){
+
+        $st=fgets($file);
+
+        if (strpos($st,'DB_PASSWORD')>-1){
+
+            return substr($st,12,-1);
+
+        }
+    }
+    return false;
+}
+
 // initialize access vars
 $host="localhost";
 $user="thinkshift";
@@ -24,17 +48,13 @@ if (mysqli_connect_errno())
     echo mysqli_connect_errno();
 };
 
-// Infusionsoft dependencies
-require_once 'src/isdk.php';
-$app=new iSDK();
-if ($debug){
-    $app->cfgCon( 'yo243', 'c959c2b28a10eb74a2d5af154ba1986d' );
-} else {
-    $app->cfgCon( 'fd341', '9122d201f6892d5b3397f675849baafa' );
-}
-
 $query="SELECT * FROM tokens;";
 $result=mysqli_query($con,$query);
 $rows=mysqli_fetch_all($result,MYSQLI_ASSOC);
 
-echo $rows[0]['Access'];
+function getToken(){
+global $rows;
+
+return $rows[0]['Access'];
+
+}
