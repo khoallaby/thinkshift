ef5bf76d64b56f973eb45aad88309ad957b381ee
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-04-19 16:05:50 -0600

    #76 - update video modals to use flat theme style.

--- platform/web/app/themes/thinkshift/templates/videos/single-video.php
+++ platform/web/app/themes/thinkshift/templates/videos/single-video.php
@@ -17,11 +17,10 @@ $link = Videos::getVideoThummbnailLink( $videoUrl, $videoSource, true );
     <div id="modal-video-<?php echo $videoId; ?>" class="modal fade modal-video">
         <div class="modal-dialog">
             <div class="modal-content">
-                <!--
                 <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                    <h4 class="modal-title"><?php the_title(); ?></h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
                 </div>
-                -->
                 <div class="modal-body">
                     <iframe src="<?php echo esc_attr( $url ); ?>" data-src="<?php echo esc_attr( $url ); ?>" frameborder="0" allowfullscreen></iframe>
                 </div>
