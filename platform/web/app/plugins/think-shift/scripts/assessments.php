7aa6a5b8f73a281da599a9a5ac7490c5a493f5d9
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-04-20 05:05:54 -0600

    #77 - update surveygizmo script to pull in new strengths with score/priority

--- platform/web/app/plugins/think-shift/scripts/assessments.php
+++ platform/web/app/plugins/think-shift/scripts/assessments.php
@@ -14,12 +14,13 @@ $debug = true;
 /*
 $_POST = [
     'user_email' => 'email@ddress.com',
-    'status' => 'completed-dream-declaration'
-    #'status' => 'completed-motivated-abilities-assessment'
+    #'status' => 'completed-dream-declaration',
+    'status' => 'completed-motivated-abilities-assessment',
+    'strength_1' => 'Generating Insight',
+    'strength_2' => 'Serving',
+    'strength_3' => 'Collaborating',
 ];
 */
-
-
 
 
 $user = get_user_by( 'email', $_POST['user_email'] );
@@ -28,22 +29,30 @@ $txt .= print_r($_POST, true) ."\n";
 
 
 if( $user ) {
-    if( $_POST['status'] ) {
-        $tags = [ $_POST['status'] ];
+    # add the completed status tag
+    $tags = [ $_POST['status'] ];
 
+    # check to see if the 3 strengths were POSTed
+    for( $i = 1; $i <= 3; $i++ ) :
+        $key = 'strength_' . $i;
         # check for strengths and add them
-        if( isset($_POST['strengths']) &&
-            !empty($_POST['strengths']) &&
+        if( isset($_POST[ $key ]) &&
+            !empty($_POST[ $key ]) &&
             $_POST['status'] == 'completed-motivated-abilities-assessment' ) {
 
-            $strengths = explode( ',', urldecode($_POST['strengths']) );
-            $tags = array_merge( $tags, $strengths );
+            $strength = urldecode( $_POST[ $key ] );
+            $tags[] = $strength;
 
-            $txt .= 'Adding strengths: ' . urldecode($_POST['strengths']) . "\n";
+            $term = get_term_by( 'name', $strength, 'tag-category' );
+            update_user_meta( $user->ID, $key, $term->term_id );
+
+            $txt .= 'Adding strength: ' . $strength . "\n";
         }
-        Users::setUserId( $user->ID );
-        Users::addUserTags( $tags, true );
-    }
+    endfor;
+
+    Users::setUserId( $user->ID );
+    Users::addUserTags( $tags, true );
+
 } else {
     $txt .= 'User not found: ' . $_POST['user_email'] . "\n";
 }
@@ -54,7 +63,7 @@ if( $user ) {
 # write to our log file
 if( $debug ) {
     # make log file writeable if not
-    if( !is_writable($file) )
-        chmod( $file, 0777 );
+    #if( !is_writable($file) )
+    #    chmod( $file, 0777 );
     file_put_contents( $file, $txt, FILE_APPEND );
 }
