c2aff40ccd87336c617314d93f71da7ab874a51f
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-05-22 17:18:56 -0600

    #111 - indent w/ 4 spaces and rename some css classes

--- platform/web/app/themes/thinkshift/templates/career/header-filter.php
+++ platform/web/app/themes/thinkshift/templates/career/header-filter.php
@@ -15,45 +15,43 @@ if( !isset($strengths) )
                   <i class="icon fa fa-filter" aria-hidden="true"></i><h6>Filter Career Cards</h6>
                 </div>
                 <div class="card-body">
-                  <div class="row">
-                    <div class="col-lg-12">
-                      <div id="filters-strength-labels" class="text-center">
-                        <h5>Filter By Strengths</h5>
-                          <?php
+                    <div class="row">
+                        <div class="col-lg-12" id="filters-strengths">
+                            <h5>Filter By Strengths</h5>
+                            <?php
 
-                          if ( isset( $_GET['strengths'] ) )
-                              $strengthArray = $_GET['strengths'];
-                          else
-                              $strengthArray = is_user_logged_in() ? array_keys( Users::getUserStrengths(2) ) : [];
+                            if ( isset( $_GET['strengths'] ) ) {
+                                $strengthArray = $_GET['strengths'];
+                            } else {
+                                $strengthArray = is_user_logged_in() ? array_keys( Users::getUserStrengths( 2 ) ) : [];
+                            }
 
-                          if ( ! $strengthArray )
-                              $strengthArray = [];
+                            if ( ! $strengthArray ) {
+                                $strengthArray = [];
+                            }
 
 
-
-                          foreach ( $strengths as $strengthId => $strength ) {
-                              $checked = in_array( $strengthId, $strengthArray );
-                              ?>
-                              <label class="<?php echo $checked ? 'active' : ''; ?>">
-                                  <?php
-                                  echo sprintf( '<input type="checkbox" %s class="" name="strengths[]" value="%s"/> %s',
-                                      $checked ? 'checked' : '',
-                                      $strengthId,
-                                      $strength
-                                  );
-                                  ?>
-                              </label>
-                          <?php } ?>
-                      </div>
+                            foreach ( $strengths as $strengthId => $strength ) {
+                                $checked = in_array( $strengthId, $strengthArray );
+                                ?>
+                                <label class="<?php echo $checked ? 'active' : ''; ?>">
+                                    <?php
+                                    echo sprintf( '<input type="checkbox" %s class="" name="strengths[]" value="%s"/> %s',
+                                        $checked ? 'checked' : '',
+                                        $strengthId,
+                                        $strength
+                                    );
+                                    ?>
+                                </label>
+                            <?php } ?>
+                        </div>
+                        <div class="col-lg-12" id="filters-education">
+                            <?php get_template_part( 'templates/career/header', 'filter-order' ); ?>
+                        </div>
+                        <div class="col-lg-12" id="filters-submit">
+                            <input type="submit" class="btn btn-primary" value="Submit"/>
+                        </div>
                     </div>
-                    <div class="col-lg-12">
-                      <div id="filters-education">
-                        <h5>Filter By Education</h5>
-                        <?php if( CustomPostTypes::isPostType( 'career' ) ) get_template_part( 'templates/career/header', 'filter-order' ); ?>
-                        <input type="submit" class="btn btn-primary" value="Submit" />
-                      </div>
-                    </div>
-                  </div>
                 </div>
             </div>
         </div>
