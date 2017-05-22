775c000e06b380d1abe57091f5145b4f52020d9a
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-05-22 17:25:22 -0600

    #111 - unindent

--- platform/web/app/themes/thinkshift/templates/career/header-filter-order.php
+++ platform/web/app/themes/thinkshift/templates/career/header-filter-order.php
@@ -14,33 +14,35 @@ $order = [
 
 ?>
 
-    <h5>Filter By Education</h5>
-    <?php foreach( $educationMin as $k => $ob ) {
-        echo sprintf( '<label><input type="checkbox" %s class="" autocomplete="off" value="%s"/> %s</label>',
-            $k,
-            isset($_GET['education_min']) ? selected( $_GET['education_min'], $k, false ) : '',
-            $ob
-        );
-    }
-    ?>
+<h5>Minimum Education Required</h5>
+<?php foreach( $educationMin as $k => $ob ) {
+    echo sprintf( '<label><input type="checkbox" %s class="" autocomplete="off" value="%s"/> %s</label>',
+        $k,
+        isset($_GET['education_min']) ? selected( $_GET['education_min'], $k, false ) : '',
+        $ob
+    );
+}
+?>
 
-    <!-- <?php foreach( $orderby as $k => $ob ) {
-        echo sprintf( '<label><input type="checkbox" %s class="" autocomplete="off" value="%s"/> %s</label>',
-            $k,
-            isset($_GET['orderby']) ? selected( $_GET['orderby'], $k, false ) : '',
-            $ob
-        );
-    }
-    ?> -->
+<!-- <?php foreach( $orderby as $k => $ob ) {
+    echo sprintf( '<label><input type="checkbox" %s class="" autocomplete="off" value="%s"/> %s</label>',
+        $k,
+        isset($_GET['orderby']) ? selected( $_GET['orderby'], $k, false ) : '',
+        $ob
+    );
+}
+?> -->
 
 
-    <h5>Order By</h5>
-    <?php foreach( $order as $k => $o ) {
-        echo sprintf( '<label><input type="checkbox" %s class="" autocomplete="off" value="%s"/> %s</label>',
-            $k,
-            isset($_GET['order']) ? selected( $_GET['order'], $k, false ) : '',
-            $o
-        );
-    }
-    ?>
+<h5>Order By</h5>
+<?php foreach( $order as $k => $o ) {
+    echo sprintf( '<label><input type="checkbox" %s class="" autocomplete="off" value="%s"/> %s</label>',
+        $k,
+        isset($_GET['order']) ? selected( $_GET['order'], $k, false ) : '',
+        $o
+    );
+}
+?>
+
+
 <?php #global $wpdb; print($wpdb->last_query); vard($wpdb); ?>
