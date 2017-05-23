94b72b201020ed1568a716e54365395ef3554adc
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-05-22 19:21:06 -0600

    #111 - no results text

--- platform/web/app/themes/thinkshift/archive-career.php
+++ platform/web/app/themes/thinkshift/archive-career.php
@@ -6,10 +6,7 @@ use ThinkShift\Plugin\Videos;
 <?php include( locate_template( 'templates/career/header-filter.php' ) ); ?>
 
 <?php if (!have_posts()) : ?>
-    <div class="alert alert-warning">
-        <?php _e('Sorry, no results were found.', 'sage'); ?>
-    </div>
-    <?php get_search_form(); ?>
+    <div class="career-card no-results"><?php _e('Sorry, no results were found.', 'thinkshift'); ?></div>
 <?php else : ?>
       <div class="col-lg-8 mt-4">
         <?php while (have_posts()) : the_post(); ?>
